06bd6ea86bebf5d43b3e8e38bae0952b3ba5402b
Minminsoh <mmsoh1@student.monash.edu>
2022-08-18 14:56:23 +1000

    adding first version of r script to analyse exercise rate by working status

--- app.R
+++ app.R
@@ -1,117 +1,174 @@
 library(shiny)
 library(tidyverse)
+library(ggplot2)
+library(maps)
+library(usmap)
+library(sf)
+
+#Import Data
+df <-read_csv("week16_exercise.csv")
+df <- df %>%
+  mutate(state = tolower(state))%>%
+  filter(state != "state_average")
+states <-sort(unique(df$state))
+
+# US map
+states_map <- map_data("state") %>% as_tibble()
+centroids <- states_map %>% group_by(region) %>% summarize(centroid_long = mean(long),
+                                                           centroid_lat = mean(lat))
+#Include Hawaii and Alaska centroids
+Hawaii_Alaska <- data.frame(region = c("alaska","hawaii"),
+                            centroid_long = c(-149.4937, -155.5828),
+                            centroid_lat = c(64.2008, 19.8968))
+centroids <- rbind(centroids, Hawaii_Alaska)
+
+#Compute average exercise rate by gender in each state
+df_gender <- df %>% select(!work_status) %>% filter(sex!= "both")
+
+#Compute average exercise rate by work status in each state
+exercise_work <- df %>%
+  filter(work_status == "working") %>%
+  group_by(state)%>%
+  summarise(average_work = mean(exercise, na.rm = TRUE))
+
+exercise_nonwork <- df %>%
+  filter(work_status == "non_working") %>%
+  group_by(state)%>%
+  summarise(average_nonwork = mean(exercise, na.rm = TRUE))
+
+df_work <- left_join(exercise_work, exercise_nonwork, by = "state") %>%
+  mutate(state = tolower(state)) %>%
+  rename(region = state)
+
+df_work_states <-left_join(states_map, df_work, by = "region")
+
+
+#plot us maps
+ggplot(df_work_states, aes(long, lat, group = region)) +
+  geom_polygon(data= states_map, aes(long, lat, group = region), fill ="grey", color ="white")+
+  geom_polygon(aes(fill = average_work), colour = "white")+
+  scale_fill_viridis_c(option = "C")+
+  geom_point(data = centroids, aes(x = centroid_long, y = centroid_lat, alpha = 0.8))+
+  geom_label(data = centroids, aes (x = centroid_long, y = centroid_lat, label = as.character(region)), size  = 2.5) +
+  theme(plot.title = element_text(hjust=0.5, face = "bold")) +
+  theme(plot.background = element_rect(fill = "white"), plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))+theme_void()
+
+ggplot() +
+  geom_polygon(data = df_work_states , aes(x = long, y = lat, group = region, fill = average_work), color = "white", fill = "grey") +
+  geom_point(data = centroids, aes(x = centroid_long, y = centroid_lat, alpha = 0.8))+
+  geom_label(data = centroids, aes (x = centroid_long, y = centroid_lat, label = as.character(region)), size  = 2)
+
+
+
 
 ui <- fluidPage(
 
-    titlePanel("Old Faithful Geyser Data"),
+  navbarPage("Average Exercise Rate in each state(s) in the United States by Gender or Work Status",
+             tabPanel("About", fluid = TRUE, icon = icon("question"),
+                      titlePanel("Background"),
+                      fluidRow(column(11,
+                                      div(class = "about",
+                                          uiOutput('about')))),
+                      includeCSS("style.css")),
 
-    h3("1. What number of bins do you stop seeing bimodality in the waiting time?"),
-    fluidRow(
-      sidebarLayout(
-          sidebarPanel(
-              sliderInput("bins",
-                          "Number of bins:",
-                          min = 1,
-                          max = 50,
-                          value = 30)
-          ),
+             tabPanel("Analysis", fluid = TRUE, icon = icon("person-walking"),
+                      titlePanel("Has working affected the average exercise rate in the United States?"),
+                      sidebarLayout(
+                        sidebarPanel(
+                          tags$style(type = "text/css", "html, body {width:100%;height:100%; font-family: Oswald, sans-serif;}"),
+                          fluidRow(column (4,
+                                           radioButtons (inputId = "Workstatus",
+                                                         label = "Select work status:",
+                                                         choices = c ("working", "non working"),
+                                                         selected = "working")
+                          ),
+                          column(5,
+                                 selectizeInput(inputId = "State",
+                                                label = "Select state(s):",
+                                                choices = states,
+                                                selected = c("alabama", "florida"),
+                                                multiple=TRUE),
+                                 helpText("Select more states which will show the entire map in US!"))
+                          )),
+                        mainPanel(
+                          fluidRow(column(3,
+                                          radioButtons(inputId = "statename",
+                                                       label = "Display:",
+                                                       choices = c("states","Neither"),
+                                                       selected = "states"))),
 
-          mainPanel(
-             plotOutput("distPlot")
-          )
-      )
-    ),
+                          plotOutput(outputId = "stateinfo"),
+                          br(),
+                          fluidRow(column(width = 3, offset = 2, actionButton("click", "Click on the button to show the bar graph comparison",icon = icon("bar-chart-o")))),
+                          fluidRow(plotOutput(outputId = "bargraph")),
+                        )
+                      ))
 
-    h3("2. How do the different geoms change the view of the data?"),
-    fluidRow(
-      sidebarLayout(
-        sidebarPanel(
-          radioButtons("geom",
-                      "Geom choice:",
-                      choices = c("geom_point",
-                                  "geom_density_2d",
-                                  "geom_density_2d_filled",
-                                  "geom_bin_2d",
-                                  "geom_hex"))
-        ),
+  ))
 
-        mainPanel(
-          plotOutput("plot")
-        )
-      )
-    ),
+server <- function(input, output, session) {
 
-    h3("3. Is a mixture of two normal distribution good fit on eruption time?"),
-    fluidRow(
-      sidebarLayout(
-        sidebarPanel(
-          sliderInput("bins2",
-                      "Adjust the number of bins (if needed):",
-                      min = 1,
-                      max = 50,
-                      value = 30),
-          "Enter your guess for the:",
-          numericInput("p", "Mixing probability:",
-                       value = 0.35, min = 0, max = 1),
-          numericInput("mean1", "Mean of the first group:",
-                       value = 2.02),
-          numericInput("mean2", "Mean of the second group:",
-                       value = 4.27),
-          numericInput("sd1", "Standard deviation of the first group:",
-                       value = 0.24, min = 0),
-          numericInput("sd2", "Standard deviation of the second group:",
-                       value = 0.44, min = 0)
-        ),
+  df_finder<-reactive({
+    filter(df_work_states, region %in% input$State)
+  }) #%>% bindEvent(input$State) #observeEvent , detailed master shiny...
 
-        mainPanel(
-          plotOutput("mixDistFit")
-        )
-      )
-    ),
+  #states_name <- reactive({
+  #  filter(states_map, region %in% input$RegionFinder)
+  #})
+
+  centroid_finder <- reactive ({
+    filter(centroids, region %in% input$State)
+  })
+
+  output$stateinfo<- renderPlot({
+    if(input$Workstatus == "working"){
+      ggplot(states_map, aes(long, lat, group = region)) +
+        geom_polygon(data= states_map, aes(long, lat, group = region), fill ="grey", color ="white")+
+        geom_polygon(data = df_finder(), aes(long, lat, group = region, fill = average_work), color = "white") + #fill use case when
+        scale_fill_viridis_c(option = "C")+
+        geom_point(data = centroid_finder(), aes(x = centroid_long, y = centroid_lat))+
+        geom_label(data = centroid_finder(), aes (x = centroid_long, y = centroid_lat, label = as.character(region)), size  = 5) +
+        labs (fill = "Average Exercise Rate")+
+        theme_void()
+    } else if (input$Workstatus == "non working"){
+      ggplot(states_map, aes(long, lat, group = region)) +
+        geom_polygon(data= states_map, aes(long, lat, group = region), fill ="grey", color ="white")+
+        geom_polygon(data = df_finder(), aes(long, lat, group = region, fill = average_nonwork), color = "white") +
+        scale_fill_viridis_c(option = "C")+
+        geom_point(data = centroid_finder(), aes(x = centroid_long, y = centroid_lat))+
+        geom_label(data = centroid_finder(), aes (x = centroid_long, y = centroid_lat, label = as.character(region)), size  = 5)+
+        labs (fill = "Average Exercise Rate")+
+        theme_void()
+
+    }
+
+  })
+
+  work_data <- reactive({
+    df_work_wide <- df_work %>% pivot_longer(cols = -region, names_to = "work_status", values_to = "avg_rate")
+    filter(df_work_wide, region %in% input$State)
+  }) %>% bindEvent(input$click) #question - doesnt change afterwards ??
 
 
-    fluidRow(
-      column(10,
-             div(class = "about",
-                 uiOutput('about'))
-      )
-    ),
-    includeCSS("styles.css")
-)
+  output$bargraph<- renderPlot({
 
-server <- function(input, output) {
+    if(input$click){
+      ggplot(data = work_data(), aes(x = reorder(region, avg_rate), y = avg_rate ))+
+        geom_col(fill = "lightblue")+
+        theme( axis.text.x = element_text(angle=45, hjust = 1)) + labs(x = "States", y = "Average Exercise Rate")+
+        facet_grid(~work_status)
+    } else {
+      ""
+    }
 
-    output$distPlot <- renderPlot({
-        ggplot(faithful, aes(waiting)) +
-         geom_histogram(bins = input$bins, color = "white") +
-         theme_bw(base_size = 14) +
-         labs(x = "Waiting time", y = "Count")
-    })
+  })%>% bindEvent(input$click)
 
-    output$plot <- renderPlot({
-      ggplot(faithful, aes(waiting, eruptions)) +
-        get(input$geom)() +
-        theme_bw(base_size = 14) +
-        labs(x = "Waiting time", y = "Eruption time")
-    })
+  output$about <- renderUI({
+    knitr::knit("about.Rmd", quiet = TRUE) %>%
+      markdown::markdownToHTML(fragment.only = TRUE) %>%
+      HTML()
+  })
 
-    output$mixDistFit <- renderPlot({
-      df <- data.frame(x = seq(min(faithful$eruptions), max(faithful$eruptions), length = 1000)) %>%
-        mutate(density = input$p * dnorm(x, input$mean1, input$sd1) +
-                         (1 - input$p) * dnorm(x, input$mean2, input$sd2))
-
-      ggplot(faithful, aes(eruptions)) +
-        geom_histogram(aes(y = stat(density)), bins = input$bins2, color = "white") +
-        geom_line(data = df, aes(x = x, y = density), color = "red", size = 2) +
-        theme_bw(base_size = 14) +
-        labs(x = "Eruption time", y = "Density")
-    })
-
-    output$about <- renderUI({
-      knitr::knit("about.Rmd", quiet = TRUE) %>%
-        markdown::markdownToHTML(fragment.only = TRUE) %>%
-        HTML()
-    })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
