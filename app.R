9061ccbf7ac216446e65d5c89194dcf91b0131d8
Minminsoh <mmsoh1@student.monash.edu>
2022-08-21 09:56:23 +1000

    update index.Rmd with shiny themes and modify plot

--- app.R
+++ app.R
@@ -1,9 +1,10 @@
 library(shiny)
+library(shinythemes)
 library(tidyverse)
 library(ggplot2)
-library(maps)
 library(usmap)
 library(sf)
+
 
 #Import Data
 df <-read_csv("data/week16_exercise.csv")
@@ -31,7 +32,11 @@ df4<- df_average %>% filter(sex== "female") %>% summarise(average_female = mean(
 df_average <- bind_cols(df1, df2,df3,df4) %>% round(digits = 0)
 
 #Compute average exercise rate by gender in each state
-df_gender <- df %>% select(!work_status) %>% filter(sex!= "both")
+df_gender <- df %>% filter(work_status == "all") %>%
+  select(state, sex,exercise) %>%
+  pivot_wider(names_from = sex, values_from = exercise)%>%
+  mutate(state = tolower(state)) %>%
+  rename(region = state)
 
 #Compute average exercise rate by work status in each state
 exercise_work <- df %>%
@@ -48,30 +53,14 @@ df_work <- left_join(exercise_work, exercise_nonwork, by = "state") %>%
   mutate(state = tolower(state)) %>%
   rename(region = state)
 
-df_work_states <-left_join(states_map, df_work, by = "region")
-
-
-#plot us maps
-ggplot(df_work_states, aes(long, lat, group = region)) +
-  geom_polygon(data= states_map, aes(long, lat, group = region), fill ="grey", color ="white")+
-  geom_polygon(aes(fill = average_work), colour = "white")+
-  scale_fill_viridis_c(option = "C")+
-  geom_point(data = centroids, aes(x = centroid_long, y = centroid_lat, alpha = 0.8))+
-  geom_label(data = centroids, aes (x = centroid_long, y = centroid_lat, label = as.character(region)), size  = 2.5) +
-  theme(plot.title = element_text(hjust=0.5, face = "bold")) +
-  theme(plot.background = element_rect(fill = "white"), plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))+theme_void()
-
-ggplot() +
-  geom_polygon(data = df_work_states , aes(x = long, y = lat, group = region, fill = average_work), color = "white", fill = "grey") +
-  geom_point(data = centroids, aes(x = centroid_long, y = centroid_lat, alpha = 0.8))+
-  geom_label(data = centroids, aes (x = centroid_long, y = centroid_lat, label = as.character(region)), size  = 2)
-
-
+# Combine dataframe with US map
+df_gw <-left_join(df_gender, df_work, by = "region")
+df_analysis <- left_join(df_gw, states_map, by = "region")
 
 
 ui <- fluidPage(
 
-  navbarPage("Average Exercise Rate in each state(s) in the United States by Gender or Work Status",
+  navbarPage("Average Exercise Rate in each state(s) in the United States by Gender or Work Status", theme = shinytheme("cerulean"),
              tabPanel("About", fluid = TRUE, icon = icon("question"),
                       titlePanel("Background"),
                       fluidRow(column(11,
@@ -82,56 +71,44 @@ ui <- fluidPage(
              tabPanel("Analysis", fluid = TRUE, icon = icon("earth-americas"),
                       titlePanel("Physical Activity - Statistics in US"),
                       sidebarLayout(sidebarPanel(
-                        fluidRow(column(10, radioButtons (inputId = "Guess1",
-                                                          label = "On average, do working population or non-working population exercise more in the United States?  ",
-                                                          choices = c("Working", "Non working"),
-                                                          inline = TRUE ),
-                                        radioButtons(inputId = "Guess2",
-                                                     label = "On average, do male or female exercise more in the United States?  ",
-                                                     choices = c("Male", "Female"),
-                                                     inline = TRUE),
-                                        actionButton("submit", "Submit your guess!"))),
+                        fluidRow(column(10, div(radioButtons (inputId = "Guess1",
+                                                              label = "On average, do working population or non-working population exercise more in the United States?  ",
+                                                              choices = c("Working", "Non working"),
+                                                              inline = TRUE ),
+                                                radioButtons(inputId = "Guess2",
+                                                             label = "On average, do male or female exercise more in the United States?  ",
+                                                             choices = c("Male", "Female"),
+                                                             inline = TRUE),
+                                                class = "guess"),
+                                        br(),
+                                        actionButton("submit", "Submit your guess!", class = "btn-danger"))),
                       ),
-                      mainPanel(textOutput("message1"),
-                                br(),
-                                textOutput("message2"))
+                      mainPanel(
+                        strong(textOutput("message1")),
+                        br(),
+                        strong(textOutput("message2")),
+                        br(),
+                        br(),
+                        textOutput("message3")),
 
                       )),
 
              tabPanel("Demographics", fluid = TRUE, icon = icon("person-walking"),
-                      titlePanel("Has working affected the average exercise rate at state(s) level in the United States?"),
+                      titlePanel("Pick a topic of interest : Working or Gender which affected the average exercise rate in the particular state(s) in the United States?"),
                       sidebarLayout(
-                        sidebarPanel(
-                          tags$style(type = "text/css", "html, body {width:100%;height:100%; font-family: Oswald, sans-serif;}"),
-                          fluidRow(column (4,
-                                           radioButtons (inputId = "Workstatus",
-                                                         label = "Select work status:",
-                                                         choices = c ("working", "non working"),
-                                                         selected = "working")
-                          ),
-                          column(5,
-                                 selectizeInput(inputId = "State",
-                                                label = "Select state(s):",
-                                                choices = states,
-                                                selected = c("alabama", "florida"),
-                                                multiple=TRUE),
-                                 helpText("Select more states which will show the entire map in US!"))
-                          )),
-                        mainPanel(
-                          fluidRow(column(3,
-                                          radioButtons(inputId = "statename",
-                                                       label = "Display:",
-                                                       choices = c("states","Neither"),
-                                                       selected = "states"))),
+                        sidebarPanel(radioButtons(inputId = "topic", label = "Select topic of interest:", choices = c("Work Status", "Gender"), inline=TRUE),
+                                     uiOutput("conditional")),
 
-                          plotOutput(outputId = "stateinfo"),
+                        mainPanel(
+                          plotOutput(outputId = "stateinfo", click = "plot_click"),
+                          textOutput("info"),
                           br(),
-                          fluidRow(column(width = 3, offset = 2, actionButton("click", "Click on the button to show the bar graph comparison",icon = icon("bar-chart-o")))),
+                          fluidRow(column(width = 3, offset = 2, actionButton("click", "Click on the button to show the bar graph comparison",icon = icon("bar-chart-o"), class = "btn-lg btn-success"))),
+
                           fluidRow(plotOutput(outputId = "bargraph")),
                         )
-                      ))
-
-  ))
+                      )
+             )))
 
 server <- function(input, output, session) {
 
@@ -152,62 +129,133 @@ server <- function(input, output, session) {
     }
   })%>%bindEvent(input$submit)
 
+  output$message3 <- renderText({
+    paste("Tips : Go to next tab for further analysis on the average exercise rate in each state(s) in the United States by Gender or Work Status")
+  })%>%bindEvent(input$submit)
 
-
+  # Third tab
   df_finder<-reactive({
-    filter(df_work_states, region %in% input$State)
+    filter(df_analysis, region %in% input$State)
   }) #%>% bindEvent(input$State) #observeEvent , detailed master shiny...
 
-  #states_name <- reactive({
-  #  filter(states_map, region %in% input$RegionFinder)
-  #})
 
   centroid_finder <- reactive ({
     filter(centroids, region %in% input$State)
   })
 
+  df_table <- reactive({
+    filter(df_gw, region %in% input$State)
+  })
+
+  output$conditional <- renderUI({
+    if(input$topic == "Work Status"){
+      tagList(
+        h4("Has working affected the average exercise rate at state(s) level in the United States?"),
+        fluidRow(column (4,
+                         radioButtons (inputId = "Workstatus",
+                                       label = "Select work status:",
+                                       choices = c ("working", "non working"),
+                                       selected = "working")
+        ),
+        column(5,
+               selectizeInput(inputId = "State",
+                              label = "Select state(s):",
+                              choices = states,
+                              selected = c("alabama", "florida"),
+                              multiple=TRUE))
+        ))} else if(input$topic == "Gender") {
+          tagList(
+            h4("Gender differences in response to average exercise rate at state(s) level in the United States"),
+            fluidRow(column (4,
+                             radioButtons (inputId = "Gender",
+                                           label = "Select gender:",
+                                           choices = c ("Male", "Female"),
+                                           selected = "Female")
+            ),
+            column(5,
+                   selectizeInput(inputId = "State",
+                                  label = "Select state(s):",
+                                  choices = states,
+                                  selected = c("alabama", "florida"),
+                                  multiple=TRUE))))
+        }
+  })
+
+
   output$stateinfo<- renderPlot({
-    if(input$Workstatus == "working"){
+
+    fill_finder <- function(fill_by){
       ggplot(states_map, aes(long, lat, group = region)) +
         geom_polygon(data= states_map, aes(long, lat, group = region), fill ="grey", color ="white")+
-        geom_polygon(data = df_finder(), aes(long, lat, group = region, fill = average_work), color = "white") + #fill use case when
-        scale_fill_viridis_c(option = "C")+
+        geom_polygon(data = df_finder(), aes(long, lat, group = region, fill = get(fill_by)), color = "white")+
         geom_point(data = centroid_finder(), aes(x = centroid_long, y = centroid_lat))+
         geom_label(data = centroid_finder(), aes (x = centroid_long, y = centroid_lat, label = as.character(region)), size  = 5) +
         labs (fill = "Average Exercise Rate")+
-        theme_void()
-    } else if (input$Workstatus == "non working"){
-      ggplot(states_map, aes(long, lat, group = region)) +
-        geom_polygon(data= states_map, aes(long, lat, group = region), fill ="grey", color ="white")+
-        geom_polygon(data = df_finder(), aes(long, lat, group = region, fill = average_nonwork), color = "white") +
-        scale_fill_viridis_c(option = "C")+
-        geom_point(data = centroid_finder(), aes(x = centroid_long, y = centroid_lat))+
-        geom_label(data = centroid_finder(), aes (x = centroid_long, y = centroid_lat, label = as.character(region)), size  = 5)+
-        labs (fill = "Average Exercise Rate")+
-        theme_void()
+        theme_void()+
+        scale_fill_viridis_c(option = "C")}
 
+    if(input$topic == "Work Status"){
+      if(input$Workstatus == "working"){
+        fill_finder("average_work")
+      }else if (input$Workstatus == "non working"){
+        fill_finder("average_nonwork")
+      }
+    }else if (input$topic == "Gender"){
+      if(input$Gender == "Male"){
+        fill_finder("male")
+      }else if (input$Gender == "Female"){
+        fill_finder("female")
+      }
     }
-
   })
 
-  work_data <- reactive({
-    df_work_wide <- df_work %>% pivot_longer(cols = -region, names_to = "work_status", values_to = "avg_rate")
-    filter(df_work_wide, region %in% input$State)
-  }) %>% bindEvent(input$click) #question - doesnt change afterwards ??
+  output$info <- renderText({
+    req(input$plot_click)
+    y <- if(input$topic == "Work Status"){
+      if(input$Workstatus == "working"){
+        df_table()$average_work
+      }else if (input$Workstatus == "non working"){
+        df_table()$average_nonwork
+      }
+    }else if (input$topic == "Gender"){
+      if(input$Gender == "Male"){
+        df_table()$male
+      }else if (input$Gender == "Female"){
+        df_table()$female
+      }
+    }
+    paste("On average, average exercise rate in", x[1,1], "is", y, "%.")
+  })
+
+
+
+  table_data <- reactive({
+    df_wide <- df_gw %>% pivot_longer(cols = -region, names_to = "demographics", values_to = "avg_rate")
+    filter(df_wide, region %in% input$State)
+  }) %>% bindEvent(input$topic, input$State)
 
 
   output$bargraph<- renderPlot({
 
     if(input$click){
-      ggplot(data = work_data(), aes(x = reorder(region, avg_rate), y = avg_rate ))+
-        geom_col(fill = "lightblue")+
+
+      ggplot(data = table_data(), aes(x = reorder(region, avg_rate), y = avg_rate, fill = demographics))+
+        geom_col()+
         theme( axis.text.x = element_text(angle=45, hjust = 1)) + labs(x = "States", y = "Average Exercise Rate")+
-        facet_grid(~work_status)
+        facet_wrap(~demographics)
+
     } else {
       ""
     }
 
-  })%>% bindEvent(input$click)
+  },  width = 600 , height = "auto", res = 96)%>% bindEvent(input$topic,input$click, input$Gender , input$Workstatus, input$State)
+
+  output$about <- renderUI({
+    knitr::knit("about.Rmd", quiet = TRUE) %>%
+      markdown::markdownToHTML(fragment.only = TRUE) %>%
+      HTML()
+  })
+
 
   output$about <- renderUI({
     knitr::knit("about.Rmd", quiet = TRUE) %>%
